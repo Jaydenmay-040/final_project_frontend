e87df60621f58a5643ecd93ad71d3646adc1f516
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2021-10-14 10:26:26 +0200

    Update explore.js

--- script/explore.js
+++ script/explore.js
@@ -198,36 +198,36 @@ function renderPosts() {
     }
   });
   
-  function renderSearch(username) {
-    fetch(`https://blooming-castle-04766.herokuapp.com/search/${username}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        window.localStorage.profile_user_id = user.user_id;
-        console.log(data);
-        users = data.users;
-        container = document.querySelector(".search_result");
-        container.innerHTML = "";
-        container.style.display = "flex";
-        users.forEach((user) => {
-          img = user.profile_img;
-          if (user.profile_img) {
-            window.localStorage.profile_view_img = user.profile_img;
-          } else {
-            window.localStorage.profile_view_img = "/images/profile.jpg";
-          }
-          container.innerHTML += `
-          <div class="resultItem">
-            <img src="${window.localStorage.profile_view_img}" alt="" class="resultImg" />
-            <p class="resultUsername">${user.username}</p>
-          </div>
-          `;
-          document.querySelectorAll(".resultItem").forEach((item) => {
-            item.addEventListener("click", (e) => {
-              window.localStorage.profile =
-                e.currentTarget.querySelector(".resultUsername").innerHTML;
-              window.location = "./profile.html";
-            });
-          });
-        });
-      });
-  }
+//   function renderSearch(username) {
+//     fetch(`https://blooming-castle-04766.herokuapp.com/search/${username}/`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         window.localStorage.profile_user_id = user.user_id;
+//         console.log(data);
+//         users = data.users;
+//         container = document.querySelector(".search_result");
+//         container.innerHTML = "";
+//         container.style.display = "flex";
+//         users.forEach((user) => {
+//           img = user.profile_img;
+//           if (user.profile_img) {
+//             window.localStorage.profile_view_img = user.profile_img;
+//           } else {
+//             window.localStorage.profile_view_img = "/images/profile.jpg";
+//           }
+//           container.innerHTML += `
+//           <div class="resultItem">
+//             <img src="${window.localStorage.profile_view_img}" alt="" class="resultImg" />
+//             <p class="resultUsername">${user.username}</p>
+//           </div>
+//           `;
+//           document.querySelectorAll(".resultItem").forEach((item) => {
+//             item.addEventListener("click", (e) => {
+//               window.localStorage.profile =
+//                 e.currentTarget.querySelector(".resultUsername").innerHTML;
+//               window.location = "./profile.html";
+//             });
+//           });
+//         });
+//       });
+//   }
